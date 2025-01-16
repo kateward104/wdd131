73c268e8c7f9d46dce446770ea706e0c64250457
kewardell14 <war22044@byui.edu>
2025-01-16 09:47:43 -0700

    finished mission assignment part 2

--- mission/mission.js
+++ mission/mission.js
@@ -1,11 +1,18 @@
 
 const themeSelector = document.querySelector("select") // replace with code to select dropdown element out of the HTML (Hint: document.querySelector)
     function changeTheme() {
-        if themeSelector == "dark":
+        const selectedTheme = themeSelector.value;
+        
+        if (selectedTheme == "dark") {
             document.body.classList.add("dark");
-
+            document.querySelector("img").src = "byui-logo_white.png";
+        }
+        else {
+            document.body.classList.remove("dark");
+            document.querySelector("img").src = "byui-logo_blue.webp";
+        }
     }
+    themeSelector.addEventListener('change', changeTheme);
 
 // add an event listener to the themeSelector element here.
 // Use the changeTheme function as the event handler function.
-themeSelector.addEventListener('change', changeTheme);
