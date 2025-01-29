183f27b2d465fce11265f65a0019ce86bccf70bc
Katelyn Wardell <kateward104@gmail.com>
2025-01-29 13:07:50 -0700

    almost finished debugging

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -3,3 +3,15 @@
 /*Selectquery (onclick) menu*/
 
 /*document.getElementById("picture").src = "new-image.jpg";*/
+
+
+
+
+const menuButton = document.getElementbyId("menu-button");
+const menuItems = document.getElementbyId("menu-items");
+
+menuButton.addEventListener("click", () => {
+    menuItems.classList.add('hide');
+});
+
+    
--- w4/debugging.js
+++ w4/debugging.js
@@ -1,13 +1,16 @@
-const pi = 3.14;
-let radius = 3;
+const PI = 3.14;
+//let radius = 3;
 let area = 0;
-area = radius * radius * pi;
-radius = 4;
-area = radius * radius * pi;
 
 function circleArea(radius) {
+    return radius * radius * PI;
+    
     // code to complete our task here
 }
+
+area = circleArea(radius);
+
+console.log("area is" + area);
 
 
 //We should have time to fix these assignments. The Cool picks you should be focusing on.
