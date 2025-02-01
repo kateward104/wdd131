943579659c322e629a2ad409447f1fbd612a93d4
Katelyn Wardell <kateward104@gmail.com>
2025-01-31 18:44:42 -0700

    added javascript

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -7,11 +7,58 @@
 
 
 
-const menuButton = document.getElementbyId("menu-button");
-const menuItems = document.getElementbyId("menu-items");
+const menuButton = document.getElementById("menu-button");
+const menuItems = document.getElementById("menu-items");
+
+
 
 menuButton.addEventListener("click", () => {
-    menuItems.classList.add('hide');
+    menuItems.classList.toggle('hide');
 });
 
-    
+
+function handleResize() {
+    const menuItems = document.getElementById("menu-items"); //This may be the issue. I don't know if I have a .menu
+    if (window.innerWidth > 1000) {
+        menuItems.classList.remove("hide");
+    } else {
+        menuItems.classList.add("hide");
+    }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
+
+
+function viewerTemplate(pic, alt) {
+    return `<div class="viewer">
+                <button class="close-viewer">X</button>
+                <img src="${pic}" alt="${alt}">
+            </div>`;
+}
+
+
+function viewHandler(event) {
+    if (event.target.tagName.toLowerCase() === "img") { // Make sure it's an image
+        const src = event.target.getAttribute("src");
+        const srcArray = src.split("-"); // Assuming the image names have a hyphen
+        const newSrc = srcArray[0] + "-full.jpeg"; // Create the full size image URL
+
+        // Insert the viewer template at the top of the body
+        document.body.insertAdjacentHTML("afterbegin", viewerTemplate(newSrc, event.target.alt));
+
+        // Add an event listener to the close button to close the viewer
+        const closeBtn = document.querySelector(".close-viewer");
+        closeBtn.addEventListener("click", closeViewer);
+    }
+}
+
+function closeViewer() {
+    const viewer = document.querySelector(".viewer");
+    if (viewer) viewer.remove();
+}
+
+
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", viewHandler);
+
