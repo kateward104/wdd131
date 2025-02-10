da46f872c2a0cdd56628b9cdf704e04b52078973
Katelyn Wardell <kateward104@gmail.com>
2025-02-10 13:35:48 -0700

    added arrays

---
+++ .vscode/arrays.js
@@ -0,0 +1,27 @@
+//  arrays.js
+const steps = ["one", "two", "three"];
+function listTemplate(step) {
+    return `<li>/${step}</li>` //the html string made from step
+}
+const stepsHtml = steps.map(listTemplate); // use map to convert the list from strings to HTML
+//    document.querySelector("#myList").innerHTML = steps.Html.join("")/// set the innerHTML
+
+const grades = ['A', 'B', 'A'];
+
+function convertGradeToNumber(grade) {
+    let point = 0
+    if (grade == 'A') {
+        point = 4
+    } else if (grade == 'B') {
+        point = 3;
+    } else if (grade == 'C') {
+        point = 2;
+    } else {
+        point = 1;
+    }
+    return point;
+}
+
+const gpaPoints = grades.map(convertGradeToNumber);
+
+console.log("gap points: " + gpaPoints);
